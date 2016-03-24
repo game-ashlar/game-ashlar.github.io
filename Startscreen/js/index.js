f39c509be4512d039eaaa0f466ebd3d3e87cb85d
AnthonyGithub <anthony.siu.email@lionsdenhk.net>
2016-03-24 11:28:40 +0800

    Added Character selection

---
+++ Startscreen/js/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  
+  $("#mage").click(function() {
+   $(this).toggleClass("enlarge");
+
+  });
+  
+})
+
+$(document).ready(function() {
+  
+  $("#archer").click(function() {
+   $(this).toggleClass("enlarge");
+
+  });
+  
+})
+
+$(document).ready(function() {
+  
+  $("#musketeer").click(function() {
+   $(this).toggleClass("enlarge");
+
+  });
+  
+})
